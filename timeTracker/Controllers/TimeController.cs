a6dc2edd3283e83be313c79953283a3f4038cf8b
jsharpe13 <58717569+jsharpe13@users.noreply.github.com>
2020-12-01 16:57:20 -0600

    final changes

--- timeTracker/Controllers/TimeController.cs
+++ timeTracker/Controllers/TimeController.cs
@@ -3,16 +3,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using timeTracker.Data;
+using timeTracker.Models;
 
 namespace timeTracker.Controllers
 {
     public class TimeController : Controller
     {
+        private ApplicationDbContext context { get; set; }
+        
+        public TimeController(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        [Route("/index")]
         public IActionResult timeIndex()
         {
             ViewBag.Month = DateTime.Now.ToString("MMMM");
             ViewBag.Year = DateTime.Now.Year;
-            return View();
+            ViewBag.Today = DateTime.Now;
+            List<Event> events = context.Events.OrderBy(x => x.start).ToList();
+            return View(events);
+        }
+
+        [Route("/add")]
+        [HttpGet]
+        public IActionResult AddEvent()
+        {
+            return View("EditEvent", new Event());
+        }
+
+        [HttpGet]
+        public IActionResult EditEvent(int id)
+        {
+            var ev = context.Events.Find(id);
+            return View(ev);
+        }
+
+        [HttpPost]
+        public IActionResult EditEvent(Event ev)
+        {
+            if(ModelState.IsValid)
+            {
+                if(ev.EventId == 0)
+                {
+                    context.Events.Add(ev);
+                }
+                else
+                {
+                    context.Events.Update(ev);
+                }
+                context.SaveChanges();
+                return RedirectToAction("timeIndex");
+            }
+            else
+            {
+                return View(ev);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var ev = context.Events.Find(id);
+            context.Events.Remove(ev);
+            context.SaveChanges();
+            return RedirectToAction("timeIndex");
         }
     }
 }
